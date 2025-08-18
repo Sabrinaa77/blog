40be00d25feb32da304afbc42e9ec25b1c19013d
sabrina <peiching913@gmail.com>
2025-08-18 15:34:45 +0800

    feat: add home and about pages with basic layout

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+app_name = 'pages'
+
+def home(request):
+  return render(request, "pages/home.html")
+
+def about(request):
+  return render(request, "pages/about.html")
