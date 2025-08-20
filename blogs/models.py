df166f95684f69269fe60960ff4cf31e7aa2695b
Sabrina <peiching913@gmail.com>
2025-08-20 12:27:09 +0800

    修正blogs頁面無法呈現文章的問題 (#9)
    * fix: correct article editing function
    
    * fix: update TIME_ZONE to Asia/Taipei
    
    * ix(blogs/view): update delete redirect to blogs:home

--- blogs/models.py
+++ blogs/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Blog(models.Model):
   title = models.CharField(max_length=200)
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
   content = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
