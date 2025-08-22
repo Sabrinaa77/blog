668440406b57324f0682f0a07ac6d8ecf69b4995
sabrina <peiching913@gmail.com>
2025-08-23 00:26:24 +0800

    style: adjust navbar alignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./templates/**/*.html",   // Django 模板
+    "./**/*.py",               // 如果 Python 中有寫 className
+    "./static/**/*.js",        // 你的前端 js
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("daisyui"),        // 如果不用 DaisyUI 可以刪掉這行
+  ],
 }
